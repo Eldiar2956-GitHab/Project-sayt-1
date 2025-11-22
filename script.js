c9dd6b162e6c98053245edada3927c53faf20b83
Eldiar2956-GitHab <bactiaroveldiar@gmail.com>
2025-11-22 18:36:04 +0600

    added full project

---
+++ script.js
@@ -0,0 +1,75 @@
+// setTimeout(()=>{
+//     document.querySelector('.Home-page').scrollIntoView({
+//         behavior:'smooth',
+//         block:'center'
+//     })
+//     setTimeout(()=>{
+//         document.querySelector('.contener2').scrollIntoView({
+//             behavior:"smooth",
+//             block:'start'
+//         })
+//         setTimeout(()=>{
+//             document.querySelector('.img-booking-bottom').scrollIntoView({
+//                 behavior:'smooth',
+//                 block:'center'
+//             })
+//             setTimeout(()=>{
+//                 document.querySelector('.contener3').scrollIntoView({
+//                     behavior:'smooth',
+//                     block:'center'
+//                 })
+//                 setTimeout(()=>{
+//                     document.querySelector('.cont4-boxs-menu').scrollIntoView({
+//                         behavior:'smooth',
+//                         block:'start'
+//                     })
+//                 setTimeout(()=>{
+//                     document.querySelector('.Hapy-puplu').scrollIntoView({
+//                         behavior:'smooth',
+//                         block:'center'
+//                     })
+//                     setTimeout(()=>{
+//                         document.querySelector('.contener-img-eag').scrollIntoView({
+//                             behavior:'smooth',
+//                             block:'center'
+//                         })
+//                             setTimeout(()=>{
+//                                 document.querySelector('.povor-info').scrollIntoView({
+//                                     behavior:'smooth',
+//                                     block:'center'
+//                                 })
+//                                     setTimeout(()=>{
+//                                         document.querySelector('.img-bottom').scrollIntoView({
+//                                             behavior:'smooth',
+//                                             block:'center'
+//                                         })
+//                                         setTimeout(()=>{
+//                                             document.querySelector('.boxs-glava').scrollIntoView({
+//                                                 behavior:'smooth',
+//                                                 block:'start'
+//                                             })
+//                                         },6000)
+//                                     },5500)
+//                                 },5000)
+//                         },4000)
+//                     },3500)
+//                 },3000)
+//             },2500)
+//         },2000)
+//     },1500)
+// },1000)
+
+// document.querySelector('.burger').addEventListener('click', function(){
+//     this.classList.toggle('activ');
+//     document.querySelector('.burger-menu').classList.toggle('open')
+// })
+
+
+
+
+
+
+
+
+
+
